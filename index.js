c92683dd0a839f05a710494e3356a964cf8e086a
Thiago <vinicius.thiago2002@gmail.com>
2023-05-06 14:51:14 -0300

    Atualização 2.0

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 "use strict";
-function aceitaNumerico(a, b) {
-    console.log(a + b);
-}
-aceitaNumerico(2, 3);
+// function aceitaNumerico (a: number, b: number)
+//     {
+//         console.log( a + b)
+//     }
+//     aceitaNumerico(2,3)
+//----STRING
+const firstName = "Thiago";
+console.log(firstName.toUpperCase());
+let fullName;
+const lastName = "Silva";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
 
-function aceitaNumerico (a: number, b: number)
-    {
-        console.log( a + b)
-    }
+// function aceitaNumerico (a: number, b: number)
+//     {
+//         console.log( a + b)
+//     }
 
-    aceitaNumerico(2,3)
+//     aceitaNumerico(2,3)
     
+
+//----STRING
+
+const firstName: string = "Thiago"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Silva"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+console.log(typeof fullName)
+
