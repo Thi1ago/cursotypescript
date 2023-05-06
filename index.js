8950c223ccb6fe02c82f76026b0e6a2067588b78
Thiago <vinicius.thiago2002@gmail.com>
2023-05-06 16:36:15 -0300

    Atualização 3.0

--- index.js
+++ index.js
@@ -5,10 +5,27 @@
 //     }
 //     aceitaNumerico(2,3)
 //----STRING
-const firstName = "Thiago";
-console.log(firstName.toUpperCase());
-let fullName;
-const lastName = "Silva";
-fullName = firstName + " " + lastName;
-console.log(fullName);
-console.log(typeof fullName);
+// const firstName: string = "Thiago"
+// console.log(firstName.toUpperCase())
+// let fullName: string
+// const lastName: string = "Silva"
+// fullName = firstName + " " + lastName
+// console.log(fullName)
+// console.log(typeof fullName)
+//-----BOOLEANO
+// let a: boolean = false
+// console.log(a)
+// console.log(typeof a)
+// a = true 
+// console.log(a)
+//----INFERENCE E ANNOTATION
+// let ann: string = "teste"
+// let inf = 2 
+// DESAFIO
+// CRIAR UMA VARIÁVEL QUE RECEBE UM NÚMERO
+let recebeNumero = 5;
+//CONVERTER ESSE NÚMERO PARA STRING EM UMA NOVA VARIÁVEL
+let numeroString = recebeNumero.toString();
+//IMPRIMIR ESTE NÚMERO EM UMA STRING MAIOR UTILIZANDO TEMPLATE OU CONCATENAÇÃO
+const printMyNumber = `O número é ${numeroString}`;
+console.log(printMyNumber);
--- index.ts
+++ index.ts
@@ -9,17 +9,46 @@
 
 //----STRING
 
-const firstName: string = "Thiago"
+// const firstName: string = "Thiago"
 
-console.log(firstName.toUpperCase())
+// console.log(firstName.toUpperCase())
 
-let fullName: string
+// let fullName: string
 
-const lastName: string = "Silva"
+// const lastName: string = "Silva"
 
-fullName = firstName + " " + lastName
+// fullName = firstName + " " + lastName
 
-console.log(fullName)
+// console.log(fullName)
 
-console.log(typeof fullName)
+// console.log(typeof fullName)
+
+
+//-----BOOLEANO
+
+
+// let a: boolean = false
+
+// console.log(a)
+// console.log(typeof a)
+
+// a = true 
+// console.log(a)
+
+//----INFERENCE E ANNOTATION
+
+// let ann: string = "teste"
+
+// let inf = 2 
+
+// DESAFIO
+
+// CRIAR UMA VARIÁVEL QUE RECEBE UM NÚMERO
+let recebeNumero: number = 5
+//CONVERTER ESSE NÚMERO PARA STRING EM UMA NOVA VARIÁVEL
+let numeroString = recebeNumero.toString()
+//IMPRIMIR ESTE NÚMERO EM UMA STRING MAIOR UTILIZANDO TEMPLATE OU CONCATENAÇÃO
+const printMyNumber: string = `O número é ${numeroString}`
+
+console.log(printMyNumber)
 
